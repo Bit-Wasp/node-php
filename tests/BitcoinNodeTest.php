74a08c048330b6e680f220ae29fb7a679a9db5cb
Thomas Kerin <afk11@users.noreply.github.com>
2016-03-20 22:03:09 +0000

    Add outstanding test classes

---
+++ tests/BitcoinNodeTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BitWasp\Bitcoin\Node\Tests;
+
+abstract class BitcoinNodeTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/Index/Validation/HeaderCheckTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace BitWasp\Bitcoin\Node\Tests\Index\Validation;
+
+use BitWasp\Bitcoin\Bitcoin;
+use BitWasp\Bitcoin\Block\BlockHeader;
+use BitWasp\Bitcoin\Block\BlockHeaderFactory;
+use BitWasp\Bitcoin\Chain\Params;
+use BitWasp\Bitcoin\Chain\ProofOfWork;
+use BitWasp\Bitcoin\Node\Consensus;
+use BitWasp\Bitcoin\Node\Index\Validation\HeaderCheck;
+use BitWasp\Bitcoin\Node\Tests\BitcoinNodeTest;
+
+class HeaderCheckTest extends BitcoinNodeTest
+{
+    private function getGenesisHex()
+    {
+        return '0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c';
+    }
+
+    /**
+     * @param $expected
+     * @param callable $callable
+     */
+    private function doAssertException($expected, callable $callable)
+    {
+        try {
+            $callable();
+            $result = true;
+        } catch (\Exception $e) {
+            $result = false;
+        }
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function assertNoException(callable $callable)
+    {
+        $this->doAssertException(true, $callable);
+    }
+
+    public function assertException(callable $callable)
+    {
+        $this->doAssertException(false, $callable);
+    }
+    
+    public function testCreateInstance()
+    {
+        $ecAdapter = Bitcoin::getEcAdapter();
+        $params = new Params($ecAdapter->getMath());
+        $consensus = new Consensus($ecAdapter->getMath(), $params);
+        $proofOfWork = new ProofOfWork($ecAdapter->getMath(), $params);
+        $check = new HeaderCheck(
+            $consensus,
+            $ecAdapter,
+            $proofOfWork
+        );
+
+        $header = BlockHeaderFactory::fromHex($this->getGenesisHex());
+        $hash = $header->getHash();
+
+        $this->assertNoException(function () use ($check, $hash, $header) {
+            $check->check($hash, $header, false);
+        });
+
+        $this->assertNoException(function () use ($check, $hash, $header) {
+            $check->check($hash, $header, true);
+        });
+        
+        $badHeader = new BlockHeader(
+            $header->getVersion(),
+            $header->getPrevBlock(),
+            $header->getMerkleRoot(),
+            $header->getTimestamp(),
+            $header->getBits(),
+            1
+        );
+
+        $this->assertException(function () use ($check, $badHeader) {
+            $hash = $badHeader->getHash();
+            $check->check($hash, $badHeader, true);
+        });
+    }
+}
